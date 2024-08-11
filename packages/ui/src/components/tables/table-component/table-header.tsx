5b286395f44770d114fb70b8f1a822557369e6a8
Prabhat Dwivedi <harsh@Prabhats-MacBook-Air.local>
2024-08-11 23:57:44 +0530

    fixed table-header bug

--- packages/ui/src/components/tables/table-component/table-header.tsx
+++ packages/ui/src/components/tables/table-component/table-header.tsx
@@ -5,27 +5,40 @@ import { CurrentTableSelector, TableHeaderSelector } from "@repo/ui/store";
 import { cn } from "@repo/ui";
 
 export const TableHeader = () => {
-  const headers = useRecoilValue(TableHeaderSelector);
   const currTable = useRecoilValue(CurrentTableSelector);
 
-  // different tables have different number number of cols required
-  let cols_num: 5 | 6;
-
   if (currTable === "Play Log") {
-    cols_num = 5;
-  } else cols_num = 6;
-
-  if (headers) {
     return (
       <div
         className={cn(
-          `grid grid-cols-${cols_num} gap-4 mb-2 py-2 border rounded-2xl border-neutral-500 text-center min-w-[1080px]`
+          `grid grid-cols-5 gap-4 mb-2 py-2 border rounded-2xl border-neutral-500 text-center min-w-[1080px]`
         )}
       >
+        <ReturnHeaders />
+      </div>
+    );
+  } else if (currTable === "Receiver Data") {
+    return (
+      <div
+        className={cn(
+          `grid grid-cols-6 gap-4 mb-2 py-2 border rounded-2xl border-neutral-500 text-center min-w-[1080px]`
+        )}
+      >
+        <ReturnHeaders />
+      </div>
+    );
+  }
+};
+
+const ReturnHeaders = () => {
+  const headers = useRecoilValue(TableHeaderSelector);
+  if (headers) {
+    return (
+      <>
         {headers.map((header, index) => (
           <div key={index}>{header}</div>
         ))}
-      </div>
+      </>
     );
   }
 };
